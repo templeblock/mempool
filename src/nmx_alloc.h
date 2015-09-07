19e4a2dc64003d90affb2b174ef46de233fc002c
Force.Charlie-I <fcharlie@users.noreply.github.com>
2015-09-07 11:46:03 +0800

    Update nmx_alloc.h

--- src/nmx_alloc.h
+++ src/nmx_alloc.h
@@ -5,8 +5,11 @@ void *nmx_alloc(size_t size);
 void *nmx_calloc(size_t size);
 void *nmx_realloc(void *p, size_t size);
 
+#ifdef _WIN32
+#define nmx_free _mm_free
+#else
 #define nmx_free          free
-
+#endif
 #define nmx_align_ptr(p, a)                                                   \
     (unsigned char *) (((unsigned int ) (p) + ((unsigned int ) a - 1)) & ~((unsigned int ) a - 1))
 
